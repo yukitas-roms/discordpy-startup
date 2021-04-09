28e6133c733953cc7472f2ba7b38dab5a16b1ad3
tommy-private <61638771+tommy-private@users.noreply.github.com>
2021-04-09 15:08:08 +0900

    schedule_heroコマンド追加
    まずは最初に表示されるメッセージを多少追加

--- discordbot.py
+++ discordbot.py
@@ -17,5 +17,21 @@ async def on_command_error(ctx, error):
 async def ping(ctx):
     await ctx.send('pong')
 
-
+    
+@bot.command()
+async def schedule_hero(ctx, date=""):
+    cnt = 12
+    tank_name = ""
+    saint_name = ""
+    msg_frame = discord.Embed(title=f"タナトスヒーロー募集 {date}", colour=0x1e90ff)
+    msg_frame.add_field(name=f"あと{cnt}人 募集中\n", value=None, inline=True)
+    msg_frame.add_field(name=f"🟥タンク：{tank_name} \n", value=None, inline=True)
+    msg_frame.add_field(name=f"🟩支援セイント：{saint_name}\n", value=None, inline=True)
+    msg_frame.add_field(name=f"🟦アーケインマスター：\n", value=None, inline=True)
+    msg_frame.add_field(name=f"🔴ルーンマスター : \n", value=None, inline=True)
+    msg = await ctx.send(embed=msg_frame)
+    await msg.add_reaction('🟥')
+    await msg.add_reaction('🟩')
+    await msg.add_reaction('🟦')
+    await msg.add_reaction('🔴')
 bot.run(token)
