0436d4e048f51d362d5b0df1c1dd076fa55c128e
tommy_private <sunyongk@gmail.com>
2021-04-11 23:19:07 +0900

    remove対応＆軽量化 ・on_raw_reaction_系に変更し、イベント発生を漏らさず取れるように ・変化のあったreactionだけ変更することとして動作速度UP ・変なreactionつけられてもエラーならないように

--- discordbot.py
+++ discordbot.py
@@ -3,11 +3,12 @@ from discord.ext import commands
 import os
 import traceback
 
-#@commands.event
-#async def on_command_error(ctx, error):
+# @commands.event
+# async def on_command_error(ctx, error):
 #    orig_error = getattr(error, "original", error)
 #    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
 #    await ctx.send(error_msg)
+
 
 # classにまとめます
 class thanatos_Cog(commands.Cog):
@@ -26,35 +27,30 @@ class thanatos_Cog(commands.Cog):
         await ctx.send('pong')
 
     @commands.command()
-    async def _update_reactions(self,msg):
-        msg = await msg.channel.fetch_message(msg.id)
+    async def _update_reactions(self, payload):
+        msg = await self.bot.get_channel(payload.channel_id).fetch_message(payload.message_id)
         embed = msg.embeds[0]
-        # 何のリアクションが来たかに関わらず、addが発生したら全リアクションを更新する
-        # 若干動作が遅いので、もしかしたら更新があったリアクションに絞った方がよいのかも
-
-        # msgに紐づく全リアクションを取ってくる
-        for r in msg.reactions:
-            id = self.marks.index(r.emoji)
-            label = "\u200b"
-            # 該当リアクションに紐づくユーザのリスト
-            users = await r.users().flatten()
-            for u in users:
-                # botでないユーザの名前を改行挟んで文字列連結
-                # 本当は上のリストをうまく絞り込んでjoinするのがpythonっぽいはずだがスキル不足である
-                if not u.bot:
-                    label += u.name + '\n'
-            embed.set_field_at(id, name=self.marks[id] + self.labels[id], value=label, inline=True)
+        # add/removeが発生したリアクションのリストを更新する
+        id = self.marks.index(payload.emoji.name)
+        label = "\u200b"
+        users = await msg.reactions[id].users().flatten()
+        for u in users:
+            # botでないユーザの名前を改行挟んで文字列連結
+            # 本当は上のリストをうまく絞り込んでjoinするのがpythonっぽいはずだがスキル不足である
+            if not u.bot:
+                label += u.name + '\n'
+        embed.set_field_at(id, name=self.marks[id] + self.labels[id], value=label, inline=True)
         await msg.edit(embed=embed)
 
     @commands.Cog.listener()
-    async def on_reaction_add(self, reaction, user):
-        if not user.bot:
-            await self._update_reactions(reaction.message)
+    async def on_raw_reaction_add(self, payload):
+        if (not payload.member.bot) & (payload.emoji.name in self.marks):
+            await self._update_reactions(payload)
 
     @commands.Cog.listener()
-    async def on_reaction_remove(self, reaction, user):
-        if not user.bot:
-            await self._update_reactions(reaction.message)
+    async def on_raw_reaction_remove(self, payload):
+        if payload.emoji.name in self.marks:
+            await self._update_reactions(payload)
 
     @commands.command()
     async def schedule_hero(self, ctx, date=""):
@@ -67,8 +63,9 @@ class thanatos_Cog(commands.Cog):
         for mark in self.marks:
             await msg.add_reaction(mark)
 
+
 if __name__ == '__main__':
-    bot = commands.Bot(command_prefix='/') # command_prefixはコマンドの最初の文字として使うもの。 e.g. !ping
+    bot = commands.Bot(command_prefix='/')  # command_prefixはコマンドの最初の文字として使うもの。 e.g. !ping
     bot.add_cog(thanatos_Cog(bot))
     token = os.environ['DISCORD_BOT_TOKEN']
     bot.run(token)
