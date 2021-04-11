38286996cbc3567fea753fdc95c6ac6bf0374706
tommy_private <sunyongk@gmail.com>
2021-04-11 17:50:36 +0900

    複数投稿対応 これまでmsgへの参照を保持していたが、リアクションに紐づくmsgを参照するように変更

--- discordbot.py
+++ discordbot.py
@@ -14,9 +14,6 @@ class thanatos_Cog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self._last_member = None
-        self.embed = discord.Embed(title="", color=0x1e90ff)
-        self.msg = None
-        self.date = ""
         # カテゴリ関係
         # 本当はclassにまとめた方がきっときれいだが、面倒なので順序を保ったリストをzipでまとめる
         self.keys = ["tank", "saint", "dark", "am", "rm", "lb", "dram", "sora", "luna", "other", "free", "absent"]
@@ -29,13 +26,14 @@ class thanatos_Cog(commands.Cog):
         await ctx.send('pong')
 
     @commands.command()
-    async def _update_reactions(self):
-        self.msg = await self.msg.channel.fetch_message(self.msg.id)
+    async def _update_reactions(self,msg):
+        msg = await msg.channel.fetch_message(msg.id)
+        embed = msg.embeds[0]
         # 何のリアクションが来たかに関わらず、addが発生したら全リアクションを更新する
         # 若干動作が遅いので、もしかしたら更新があったリアクションに絞った方がよいのかも
 
         # msgに紐づく全リアクションを取ってくる
-        for r in self.msg.reactions:
+        for r in msg.reactions:
             id = self.marks.index(r.emoji)
             label = "\u200b"
             # 該当リアクションに紐づくユーザのリスト
@@ -45,30 +43,29 @@ class thanatos_Cog(commands.Cog):
                 # 本当は上のリストをうまく絞り込んでjoinするのがpythonっぽいはずだがスキル不足である
                 if not u.bot:
                     label += u.name + '\n'
-            self.embed.set_field_at(id, name=self.marks[id] + self.labels[id], value=label, inline=True)
-        await self.msg.edit(embed=self.embed)
+            embed.set_field_at(id, name=self.marks[id] + self.labels[id], value=label, inline=True)
+        await msg.edit(embed=embed)
 
     @commands.Cog.listener()
     async def on_reaction_add(self, reaction, user):
         if not user.bot:
-            await self._update_reactions()
+            await self._update_reactions(reaction.message)
 
     @commands.Cog.listener()
     async def on_reaction_remove(self, reaction, user):
         if not user.bot:
-            await self._update_reactions()
+            await self._update_reactions(reaction.message)
 
     @commands.command()
     async def schedule_hero(self, ctx, date=""):
         # 最初の描画
-        self.date = date
-        self.embed.title = f"タナトスヒーロー募集 ： {self.date} "
+        embed = discord.Embed()
+        embed.title = f"タナトスヒーロー募集 ： {date} "
         for key, mark, label in zip(self.keys, self.marks, self.labels):
-            self.embed.add_field(name=mark+label, value="\u200b", inline=True)
-        self.msg = await ctx.send(embed=self.embed)
+            embed.add_field(name=mark+label, value="\u200b", inline=True)
+        msg = await ctx.send(embed=embed)
         for mark in self.marks:
-            await self.msg.add_reaction(mark)
-
+            await msg.add_reaction(mark)
 
 if __name__ == '__main__':
     bot = commands.Bot(command_prefix='/') # command_prefixはコマンドの最初の文字として使うもの。 e.g. !ping
